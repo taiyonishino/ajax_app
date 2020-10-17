be129331edb7e9353b3a58d6e7145e779a5ae07a
taiyonishino <taiyo0807@icloud.com>
2020-10-18 00:06:44 +0900

    AjaxAppの準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'tweets#index'
-  resources :tweets, only: [:index, :new, :create, :destroy, :edit]
+  root to: 'posts#index' 
+# get 'posts/new', to: 'posts#new' ←削除
+  post 'posts', to: 'posts#create'
 end
